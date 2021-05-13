65a44cf190d19d7d70702ea22514766f5827fa4a
Mario <marioandreseche@gmail.com>
2021-05-13 19:57:16 -0400

    confirmacion de lectrua folio

--- app.js
+++ app.js
@@ -43,9 +43,8 @@ app.post('/guardarArchivo', async function (request, response) {
 
 app.post('/confirmarLectura', async function (request, response) {
   let nombreCarpeta = request.body.folio.nombreCarpetaAws;
-  console.log(nombreCarpeta)
   /* let nombreArchivo = request.body.folio.correlativo+"-"+request.body.folio.asunto+"-"+request.body.folio.libro.contrato.nombre; */
-  let nombreArchivo = `Folio Nº${request.body.folio.correlativo}_${request.body.folio.libro.nombre}-lectura`;
+  let nombreArchivo = `Folio Nº${request.body.folio.correlativo}_${request.body.folio.libro.nombre}_confirmación`;
   let respuesta = await aws.guardarArchivo(nombreCarpeta, nombreArchivo, request.body.pdf);
   if(respuesta){
     return response.send(JSON.stringify(respuesta));
